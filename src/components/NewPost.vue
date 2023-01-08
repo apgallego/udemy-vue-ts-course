87c26afce42537836b72bc6e33c6f6a52a9e8b5e
Alfredo Puerta Gallego <alfredo.puertagallego.st@emeal.nttdata.com>
2023-01-08 22:49:29 +0100

    Routing and DOM access

---
+++ src/components/NewPost.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import PostWriter from './PostWriter.vue';
+import { TimelinePost } from "../posts";
+import { DateTime } from 'luxon';
+
+const post: TimelinePost = {
+    id: "-1",
+    title: "title",
+    created: DateTime.now()
+};
+
+</script>
+
+<template>
+    New Post
+    <PostWriter :post="post" />
+</template>
---
+++ src/components/PostWriter.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { TimelinePost } from '../posts';
+
+const props = defineProps<{
+    post: TimelinePost
+}>();
+
+const title = ref(props.post.title);
+const contentEditable = ref<HTMLDivElement>();
+
+console.log(contentEditable.value?.innerText);
+
+onMounted(() => {
+    console.log("On mounted");
+    console.log(contentEditable.value?.innerText);
+});
+</script>
+
+<template>
+    <div class="columns">
+        <div class="column">
+            <div class="field">
+                <div class="label">Post title</div>
+                <input type="text" class="input" v-model="title">
+            </div>
+            <div contenteditable ref="contentEditable">
+                This is the post.
+            </div>
+        </div>
+    </div>
+</template>
