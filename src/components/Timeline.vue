12ee99ff98bc38bdee96a904874e3ea76f2c78c1
Alfredo Puerta Gallego <alfredo.puertagallego.st@emeal.nttdata.com>
2023-01-08 20:34:51 +0100

    composition API and pinia ( +expressed installed)

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,59 +1,26 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
-import { DateTime } from "luxon";
-import { TimelinePost, today, thisWeek, thisMonth } from "../posts";
 import TimelineItem from "./TimelineItem.vue";
 import { usePosts } from "../stores/posts";
+import { periods } from "../constants";
 
 const postsStore = usePosts();
-
-const periods = ["Today", "This Week", "This Month"] as const;
-type Period = typeof periods[number];
-
-const selectedPeriod = ref<Period>("Today");
-
-const selectPeriod = (period: Period) => {
-  selectedPeriod.value = period;
-};
-
-const posts = computed<TimelinePost[]>(() => {
-    return [today, thisWeek, thisMonth]
-    .map(post => {
-    return{
-            ...post,
-            created: DateTime.fromISO(post.created)
-        }
-    })
-    .filter(post => {
-        if(selectedPeriod.value === "Today") {
-            return post.created >= DateTime.now().minus({ day: 1 })
-        }
-        if(selectedPeriod.value === "This Week") {
-            return post.created >= DateTime.now().minus({ week: 1 })
-        }
-
-        return post;
-    });
-});
 </script>
 
 <template>
-    {{ postsStore.getState().foo }}
-    <button @click="postsStore.updateFoo('bar')">Update</button>
   <nav class="is-primary panel">
     <span class="panel-tabs">
       <a
         v-for="period of periods"
         :key="period"
-        :class="{ 'is-active': period === selectedPeriod }"
-        @click="selectPeriod(period)"
+        :class="{ 'is-active': period === postsStore.selectedPeriod }"
+        @click="postsStore.setSelectedPeriod(period)"
       >
         {{ period }}
       </a>
     </span>
 
     <TimelineItem 
-        v-for="post of posts"
+        v-for="post of postsStore.filtederPosts"
         :key="post.id"
         :post="post"
     />
--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -1,34 +1,56 @@
-import { reactive, readonly } from "vue";
-
-// ref number, string
-// computed
-// reactive {}, Map, Set
+import { defineStore } from "pinia";
+import { Period } from "../constants";
+import { Post, today, thisWeek, thisMonth, TimelinePost } from "../posts";
+import { DateTime } from "luxon";
 
 interface PostsState {
-    foo: string;
+    ids: string[];
+    all: Map<string, Post>;
+    selectedPeriod: Period;
 };
 
-export class PostsStore {
-    //#: for private properties
-    #state: PostsState;
+export const usePosts = defineStore("posts", {
+    state: (): PostsState => ({
+        ids: [today.id, thisWeek.id, thisMonth.id],
+        all: new Map([
+            [today.id, today],
+            [thisWeek.id, thisWeek],
+            [thisMonth.id, thisMonth],
+        ]),
+        selectedPeriod: "Today"
+    }),
 
-    constructor() {
-        this.#state = reactive<PostsState>({
-            foo: "foo"
-        })
+    actions: {
+        setSelectedPeriod (period: Period){
+            this.selectedPeriod = period;
+        }
+    },
+
+    getters: {
+        filtederPosts: (state): TimelinePost[] => {
+            return state.ids
+            .map(id => {
+            const post = state.all.get(id);
+
+            if(!post) {
+                throw Error(`Post with id of ${id} was expected but not found.`);
+            }
+
+            return {
+                ...post,
+                created: DateTime.fromISO(post.created)
+            }
+            })
+            .filter(post => {
+                if(state.selectedPeriod === "Today") {
+                    return post.created >= DateTime.now().minus({ day: 1 })
+                }
+                if(state.selectedPeriod === "This Week") {
+                    return post.created >= DateTime.now().minus({ week: 1 })
+                }
+
+                return post;
+            });
+        }
     }
-
-    getState(){
-        return readonly(this.#state);
-    }
-
-    updateFoo(foo: string) {
-        this.#state.foo = foo;
-    }
-};
-
-const store = new PostsStore();
-
-export function usePosts() {
-    return store;
-}
+});
